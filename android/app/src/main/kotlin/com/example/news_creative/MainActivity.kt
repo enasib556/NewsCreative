eccaf43e630b9fb27a7b58b6619fcffe30c411ea
enas ibrahim <enasib556.com>
2025-02-15 11:11:13 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/news_creative/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.news_creative
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/model/catgory_model.dart
@@ -0,0 +1,6 @@
+class CatgoryModel
+{
+ final String imageUrl;
+ final String title;
+ const CatgoryModel({ required this.imageUrl, required this.title});
+}
